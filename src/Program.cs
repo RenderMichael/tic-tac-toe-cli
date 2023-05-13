05b9fba5453755a3348febefd0f18bd24b261032
Michael Render <render.michael@gmail.com>
2023-05-12 22:47:46 -0400

    Add --help support
    Also add error handling for bad args

--- src/Program.cs
+++ src/Program.cs
@@ -1,3 +1,22 @@
+using System.Runtime.CompilerServices;
 using Michael.TicTacToe;
 
-TicTacToePlayer.Default.PlayGame();
+if (args.Length == 0)
+{
+    TicTacToePlayer.Default.PlayGame();
+}
+else if (args.Length == 1 && args[0] is "--help" or "-h" or "-?")
+{
+    TicTacToeHelpDisplayer.DisplayHelpMessage();
+}
+else
+{
+    LogErrorMessage(args);
+}
+
+[MethodImpl(MethodImplOptions.NoInlining)] // Cold path
+static void LogErrorMessage(string[] args)
+{
+    Console.WriteLine($"Unknown subcommand(s): {string.Join(' ', args)}");
+    Console.WriteLine("Run the --help command for more info");
+}
---
+++ src/TicTacToeHelpDisplayer.cs
@@ -0,0 +1,16 @@
+namespace Michael.TicTacToe;
+
+public static class TicTacToeHelpDisplayer
+{
+    public static void DisplayHelpMessage()
+    {
+        var help =
+            "Description:" + Environment.NewLine +
+            "  Plays a game of Tic-Tac-Toe." + Environment.NewLine + Environment.NewLine +
+            "Usage:" + Environment.NewLine +
+            "  dotnet run" + Environment.NewLine + Environment.NewLine +
+            "Options:" + Environment.NewLine +
+            "  -?, -h, --help                       Show command line help." + Environment.NewLine;
+        Console.WriteLine(help);
+    }
+}
