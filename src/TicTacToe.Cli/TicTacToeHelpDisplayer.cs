41925833ea27847088b738e13c714afdfbb380b2
Michael Render <render.michael@gmail.com>
2023-06-08 15:55:45 -0400

    Use .NET 7 features in CLI project

--- src/TicTacToe.Cli/TicTacToeHelpDisplayer.cs
+++ src/TicTacToe.Cli/TicTacToeHelpDisplayer.cs
@@ -4,13 +4,14 @@ public static class TicTacToeHelpDisplayer
 {
     public static void DisplayHelpMessage()
     {
-        var help =
-            "Description:" + Environment.NewLine +
-            "  Plays a game of Tic-Tac-Toe. Controls are the with the number pad." + Environment.NewLine + Environment.NewLine +
-            "Usage:" + Environment.NewLine +
-            "  dotnet run" + Environment.NewLine + Environment.NewLine +
-            "Options:" + Environment.NewLine +
-            "  -?, -h, --help                       Show command line help." + Environment.NewLine;
+        var help = $"""
+            Description:{Environment.NewLine}
+              Plays a game of Tic-Tac-Toe. Controls are the with the number pad.{Environment.NewLine}{Environment.NewLine}
+            Usage:{Environment.NewLine}
+              dotnet run{Environment.NewLine}{Environment.NewLine}
+            Options:{Environment.NewLine}
+              -?, -h, --help                       Show command line help.{Environment.NewLine}
+            """;
         Console.WriteLine(help);
     }
 }
